6824ec2981725fc659181a7ffca0984a78fdeb18
paulaentringer <paulaetgr92@gmail.com>
2025-05-28 18:19:40 -0300

    mensagem

--- school-system/routes/api_rooutes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify 
-
-api_dp = Blueprint("api_dp", __name__)
-
-@api_dp.route('/', methods=['GET'])
-def api_root(): 
-    return jsonify({"message": "API is running"}), 200
